63548200c373cc43bee74ed6daf76f96a8887bed
iceTea-NHDYH <istinurh1312.02@gmail.com>
2023-02-23 21:01:26 +0800

    vrs4

--- main.py
+++ main.py
@@ -18,6 +18,6 @@ print("**********")
 
 headers = {'User-Agent':'Mobile'}
 
-url2 = 'http://172.18.58.80/spicyx/'
+url2 = 'http://httpbin.org/headers'
 rh = requests.get(url2, headers=headers)
 print(rh.text)
