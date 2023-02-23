ed28e0c595b57f88a6dad7f67435e50dfe6bb84a
iceTea-NHDYH <istinurh1312.02@gmail.com>
2023-02-23 20:46:15 +0800

    vrs4

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
-import scrapy
-class NewSpider(scrapy.Spider):
-    name = "new.spider"
-    start_urls = ['http://http://172.18.58.80/spicyx/']
+import requests
+
+url = 'http://172.18.58.80/spicyx/'
+r = requests.get(url)
+
+print(r.text)
+
+print("Status code:")
+print("\t *", r.status_code)
+
+h = requests.head(url)
+print("Header:")
+print("**********")
+
+for x in h.headers:
+    print("\t ", x, ":", h.headers[x])
+print("**********")
+
+headers = {'User-Agent' :'Mobile'}
+
+url2 = 'http://httpbin.org/headers'
+rh = requests.get(url2, headers=headers)
+print(rh.text)
